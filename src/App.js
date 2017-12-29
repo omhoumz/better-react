875eae2eae92d97ef1dbf867d17cb52b8cb50baa
Omar Houmz <omarhoumz@gmail.com>
2017-12-29 15:05:16 +0000

    Adds fake server data: * Fake data is just an object defined at App.js * Populates title and agregates with the fake data * Adds placeholders images

--- src/App.js
+++ src/App.js
@@ -4,12 +4,107 @@ import './App.css';
 let defaultStyle = {
   display: "inline-block"
 }
+let image = {
+  sm: "http://via.placeholder.com/20x20",
+  md: "http://via.placeholder.com/120x120"
+}
+let fakeServerData = {
+  user: {
+    name: "Omar",
+    playlists: [
+      {
+        name: "My Favorites",
+        songs: [
+          {
+            name: 'Le song',
+            duration: 1210
+          },
+          {
+            name: 'Une chançon',
+            duration: 3200
+          },
+          {
+            name: 'Rosa Heliker',
+            duration: 2510
+          }
+        ]
+      },
+      {
+        name: "Discover Weekly",
+        songs: [
+          {
+            name: 'Le song',
+            duration: 1210
+          },
+          {
+            name: 'Une chançon',
+            duration: 3200
+          },
+          {
+            name: 'Rosa Heliker',
+            duration: 2510
+          }
+        ]
+      },
+      {
+        name: "Most viewed",
+        songs: [
+          {
+            name: 'Le song',
+            duration: 1210
+          },
+          {
+            name: 'Une chançon',
+            duration: 3200
+          },
+          {
+            name: 'Rosa Heliker',
+            duration: 2510
+          }
+        ]
+      },
+      {
+        name: "Best Tracks",
+        songs: [
+          {
+            name: 'Le song',
+            duration: 1210
+          },
+          {
+            name: 'Une chançon',
+            duration: 3200
+          },
+          {
+            name: 'Rosa Heliker',
+            duration: 2510
+          }
+        ]
+      }
+    ]
+  }
+}
 
-class Agregate extends Component {
+class PlaylistCounter extends Component {
   render() {
     return (
       <div style={ {width: '40%', display: "inline-block" } }>
-        <h2>Number Text</h2>
+        <h2>{this.props.playlists.length} Playlists</h2>
+      </div>
+    );
+  }
+}
+
+class HoursCounter extends Component {
+  render() {
+    let allSongs = this.props.playlists.reduce((songs, eachPlaylist) => {
+      return songs.concat(eachPlaylist.songs)
+    }, [])
+    let totalDuration = allSongs.reduce((sum, eachSong) => {
+      return sum + eachSong.duration
+    }, 0)
+    return (
+      <div style={ {width: '40%', display: "inline-block" } }>
+        <h2>{Math.round(totalDuration/60/60)} hours</h2>
       </div>
     );
   }
@@ -18,8 +113,8 @@ class Agregate extends Component {
 class Filter extends Component {
   render() {
     return (
-      <div style={{...defaultStyle, width: '25%'}} >
-        <img alt="icon" />
+      <div style={{...defaultStyle, width: '40%'}} >
+        <img alt="icon" src={image.sm} style={{...defaultStyle, margin: "0 10px -6px"}} />
         <input type="text" />
       </div>
     );
@@ -30,7 +125,7 @@ class Playlist extends Component {
   render() {
     return (
       <div style={{...defaultStyle, width: '25%'}} >
-        <img alt="image" />
+        <img alt="image" src={image.md} />
         <h3>Playlist Name</h3>
         <ul>
           <li>Song 1</li>
@@ -43,25 +138,41 @@ class Playlist extends Component {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {serverData: {}}
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({serverData: fakeServerData})
+    }, 200);
+  }
   render() {
-    let headerStyle = {"font-size": "50px"}
+    let headerStyle = {fontSize: "50px"}
     return (
       <div className="App">
-        <header>
-          <h1 style={headerStyle} >Title</h1>
-        </header>
-        <section>
-          <Agregate />
-          <Agregate />
-          <Filter />
-        </section>
-        <div style={{height: "60px"}} ></div>
-        <section>
-          <Playlist />
-          <Playlist />
-          <Playlist />
-          <Playlist />
-        </section>
+        { this.state.serverData.user ?
+
+          <div className="appContent">
+            <header>
+              <h1 style={headerStyle} >
+                { this.state.serverData.user.name }'s Playlists
+              </h1>
+              
+              <PlaylistCounter playlists={ this.state.serverData.user.playlists } />
+              <HoursCounter playlists={ this.state.serverData.user.playlists } />
+              <Filter />
+            </header>
+            <div style={{height: "60px"}} ></div>
+            <section>
+              <Playlist />
+              <Playlist />
+              <Playlist />
+              <Playlist />
+            </section>
+          </div> : <h1>Loading ...</h1>
+
+        }
       </div>
     );
   }
